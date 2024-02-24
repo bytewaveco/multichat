a516e6015ac3a0ade8298e9e81bcd2ba2776549b
Kaden Griffith <kadengriffith@gmail.com>
2024-02-23 21:31:30 -0700

    chore: update server and client test

--- client.js
+++ client.js
@@ -1,40 +1,57 @@
-const dgram = require("dgram");
-const client = dgram.createSocket("udp4");
-let messageQueue = [];
-let currentSequenceNumber = 0;
+// const dgram = require("dgram");
+// const client = dgram.createSocket("udp4");
+// let messageQueue = [];
+// let currentSequenceNumber = 0;
 
-function sendMessage(message, address, port) {
-  const messageWithSeq = JSON.stringify({
-    seq: currentSequenceNumber,
-    ...message,
+// function sendMessage(message, address, port) {
+//   const messageWithSeq = JSON.stringify({
+//     seq: currentSequenceNumber,
+//     ...message,
+//   });
+//   client.send(
+//     messageWithSeq,
+//     0,
+//     messageWithSeq.length,
+//     port,
+//     address,
+//     (err) => {
+//       if (err) {
+//         console.error(err);
+//       } else {
+//         console.log(`Message sent: ${messageWithSeq}`);
+//         messageQueue.push({
+//           seq: currentSequenceNumber,
+//           message: messageWithSeq,
+//         });
+//         currentSequenceNumber++;
+//       }
+//     }
+//   );
+// }
+
+// // Logic to handle ACKs and resend missing messages
+// client.on("message", (msg) => {
+//   const ack = JSON.parse(msg.toString());
+//   console.log(`ACK received for: ${ack.seq}`);
+//   messageQueue = messageQueue.filter((m) => m.seq !== ack.seq);
+// });
+
+// // Example usage
+// sendMessage({ data: "Hello, World" }, "234.1.1.1", 4321);
+
+const dgram = require('dgram');
+const client = dgram.createSocket({ type: 'udp4', reuseAddr: true });
+
+const message = Buffer.from('{ "hello": "world" }');
+const multicastAddress = '234.1.1.1';
+const port = 4321;
+
+client.bind(function() {
+  client.setBroadcast(true);
+  client.setMulticastTTL(128);
+  client.send(message, 0, message.length, port, multicastAddress, function(err) {
+    if (err) console.error(err);
+    console.log('Multicast message sent');
+    client.close();
   });
-  client.send(
-    messageWithSeq,
-    0,
-    messageWithSeq.length,
-    port,
-    address,
-    (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(`Message sent: ${messageWithSeq}`);
-        messageQueue.push({
-          seq: currentSequenceNumber,
-          message: messageWithSeq,
-        });
-        currentSequenceNumber++;
-      }
-    }
-  );
-}
-
-// Logic to handle ACKs and resend missing messages
-client.on("message", (msg) => {
-  const ack = JSON.parse(msg.toString());
-  console.log(`ACK received for: ${ack.seq}`);
-  messageQueue = messageQueue.filter((m) => m.seq !== ack.seq);
 });
-
-// Example usage
-sendMessage({ data: "Hello, World" }, "234.1.1.1", 4321);
--- server.js
+++ server.js
@@ -1,25 +1,40 @@
-const dgram = require("dgram");
-const server = dgram.createSocket("udp4");
+// const dgram = require("dgram");
+// const server = dgram.createSocket("udp4");
 
-server.on("message", (msg, rinfo) => {
-  const message = JSON.parse(msg.toString());
-  console.log(`Received: ${msg.toString()}`);
-  // Send ACK back
-  const ack = JSON.stringify({ seq: message.seq });
-  server.send(ack, 0, ack.length, rinfo.port, rinfo.address, (err) => {
-    if (err) console.error(err);
-    else console.log(`ACK sent for: ${message.seq}`);
-  });
+// server.on("message", (msg, rinfo) => {
+//   const message = JSON.parse(msg.toString());
+//   console.log(`Received: ${msg.toString()}`);
+//   // Send ACK back
+//   const ack = JSON.stringify({ seq: message.seq });
+//   server.send(ack, 0, ack.length, rinfo.port, rinfo.address, (err) => {
+//     if (err) console.error(err);
+//     else console.log(`ACK sent for: ${message.seq}`);
+//   });
+// });
+
+// server.bind({
+//   address: "0.0.0.0",
+//   port: 4321,
+//   exclusive: false,
+// });
+
+// server.on("listening", () => {
+//   server.setBroadcast(true);
+//   server.setMulticastTTL(128);
+//   server.addMembership("234.1.1.1", "0.0.0.0");
+// });
+
+const dgram = require('dgram');
+const server = dgram.createSocket({ type: 'udp4', reuseAddr: true });
+
+const multicastAddress = '234.1.1.1';
+const port = 4321;
+
+server.bind(port, function() {
+  server.addMembership(multicastAddress);
+  console.log(`Server listening for multicast messages on ${multicastAddress}:${port}`);
 });
 
-server.bind({
-  address: "0.0.0.0",
-  port: 4321,
-  exclusive: false,
-});
-
-server.on("listening", () => {
-  server.setBroadcast(true);
-  server.setMulticastTTL(128);
-  server.addMembership("234.1.1.1", "0.0.0.0");
+server.on('message', function(message, remote) {
+  console.log(`Received multicast message from ${remote.address}:${remote.port}: ${message}`);
 });
